bda3ef7f0ee81718afc35ba85e22562eeac10da7
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-24 14:20:43 +0300

    Create some properties for class

--- src/main/java/com/predoana/app/chat/model/ChatInMessage.java
+++ src/main/java/com/predoana/app/chat/model/ChatInMessage.java
@@ -1,4 +1,43 @@
 package com.predoana.app.chat.model;
 
+import java.util.Date;
+
 public class ChatInMessage {
+
+    private String senderId;
+    private String senderName;
+    private String message;
+    private Date timestamp;
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 }
