90ee918d76a0306e63aa2e12262473bd44ed8137
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-25 11:52:38 +0300

    Messages get from the server

--- src/main/java/com/predoana/app/chat/model/ChatOutMessage.java
+++ src/main/java/com/predoana/app/chat/model/ChatOutMessage.java
@@ -1,4 +1,34 @@
 package com.predoana.app.chat.model;
 
+import java.util.Date;
+
 public class ChatOutMessage {
+
+    private String content;
+    private String groupName;
+    private Date timestamp;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 }
