01f424664415a3a60cbda64dafb6b005e48813a8
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-25 11:56:04 +0300

    Representations for messaging participants

--- src/main/java/com/predoana/app/chat/model/ChatParticipant.java
+++ src/main/java/com/predoana/app/chat/model/ChatParticipant.java
@@ -1,4 +1,41 @@
 package com.predoana.app.chat.model;
 
 public class ChatParticipant {
+
+    private String firstName;
+    private String lastName;
+    private String shortName;
+    private String participantType ;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getParticipantType() {
+        return participantType;
+    }
+
+    public void setParticipantType(String participantType) {
+        this.participantType = participantType;
+    }
 }
