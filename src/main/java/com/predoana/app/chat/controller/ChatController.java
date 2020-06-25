bc9ffa2e44fe2f41e8451a8b203a52ed5dfa815c
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-25 12:13:23 +0300

    Mapping handler method using @MessageMapping

--- src/main/java/com/predoana/app/chat/controller/ChatController.java
+++ src/main/java/com/predoana/app/chat/controller/ChatController.java
@@ -1,11 +1,16 @@
 package com.predoana.app.chat.controller;
 
+
 import com.predoana.app.chat.model.ChatInMessage;
 import com.predoana.app.chat.model.ChatOutMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 
-
+@Controller
 public class ChatController {
 
+    @MessageMapping("/guestchat")
     public ChatOutMessage handleMessaging(ChatInMessage message) throws Exception {
         Thread.sleep(1000); // simulate delay
 
