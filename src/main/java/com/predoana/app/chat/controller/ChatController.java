4c9b27db46f7fc0062953a118f532fb13e321b3e
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-25 12:06:40 +0300

    Add a delay;

--- src/main/java/com/predoana/app/chat/controller/ChatController.java
+++ src/main/java/com/predoana/app/chat/controller/ChatController.java
@@ -3,9 +3,12 @@ package com.predoana.app.chat.controller;
 import com.predoana.app.chat.model.ChatInMessage;
 import com.predoana.app.chat.model.ChatOutMessage;
 
+
 public class ChatController {
 
     public ChatOutMessage handleMessaging(ChatInMessage message) throws Exception {
-        return new ChatOutMessage();
+        Thread.sleep(1000); // simulate delay
+
+        return new ChatOutMessage(message.getMessage());
     }
 }
