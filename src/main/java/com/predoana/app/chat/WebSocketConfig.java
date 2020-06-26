9982feabe69b59dc7ce164bc563d3184c4cc15e8
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-26 11:27:47 +0300

    Implemented registrerStompEndpoints().

--- src/main/java/com/predoana/app/chat/WebSocketConfig.java
+++ src/main/java/com/predoana/app/chat/WebSocketConfig.java
@@ -1,6 +1,17 @@
 package com.predoana.app.chat;
 
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat-app").withSockJS();
+    }
+
+
+
+
 }
