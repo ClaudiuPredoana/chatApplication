9a6765f8ab4306ee8970b962c1067f07903952f5
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-26 11:21:50 +0300

    Create the message broker

---
+++ src/main/java/com/predoana/app/chat/WebSocketConfig.java
@@ -0,0 +1,6 @@
+package com.predoana.app.chat;
+
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+}
